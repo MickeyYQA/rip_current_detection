e25b0b37ad12d762149d61b6e44e74a44a4aed59
you7n <68904595+MickeyYQA@users.noreply.github.com>
2024-06-30 18:31:24 +0800

    Update rip-model-train.py

--- rip-model-train.py
+++ rip-model-train.py
@@ -5,12 +5,14 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, classification_report
 import pickle
 import time
+from colorama import Fore, Back, Style
 
 # import data
-train_data = pd.read_csv('csv_data/all_pics.csv')
-test_data = pd.read_csv('with_rips_100.csv')
+train_data = pd.read_csv('splitted_csv/train-tsht.csv')
+test_data = pd.read_csv('splitted_csv/test.csv')
 
 # data convertion
 Train_data = train_data.values
@@ -37,27 +39,38 @@ y_test = y_test[~nan_rows_test]
 print("X_train shape:", X_train.shape)
 print("X_test shape:", X_test.shape)
 
+train_begin = time.time()
 # train model
 model = MLPClassifier(solver='adam',
                       hidden_layer_sizes=(60, ),
                       random_state=1, max_iter=150)
 model.fit(X_train, y_train)
+train_end = time.time()
 
 # use pickle to export model
-with open('model_100.pkl', 'wb') as file:
+with open('model_splitted.pkl', 'wb') as file:
     pickle.dump(model, file)
 
-# start timing
 begin = time.time()
+print("training time:",(train_end - train_begin) * 10**3 ,"ms")
 
 # evaluate model
 train_score = model.score(X_train, y_train)
 test_score = model.score(X_test, y_test)
-
-# show results
-print('训练集的准确率：%f'%train_score,"%")
-print('测试集的准确率：%f'%test_score,"%")
-
-# end timing
 end = time.time()
+probs = model.predict_proba(X_test)
+'''
+with open('probabilities.txt', 'w') as f:
+    for sample_probs in probs:
+        sample_probs_str = ','.join(map(str, sample_probs))
+        f.write(sample_probs_str + '\n')
+'''
+# show results
+intend = input("Intend for this test:")
+print(Fore.RED + intend)
+print('Accuracy on training set: %f'%train_score)
+print('Accuracy on testing set: %f'%test_score)
 print("run time:",(end - begin) * 10**3 ,"ms")
+
+pos_predictions = model.predict(X_test)
+print(classification_report(y_test, pos_predictions, zero_division = 0))
